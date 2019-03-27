64f514176c327a8b43354b46bf49c4a653dc21c3
devmeireles <dev.meireles@gmail.com>
2019-03-27 17:29:41 +0000

    :pencil2: add functios

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -5,81 +5,28 @@ namespace App\Http\Controllers;
 use App\Categorias;
 use Illuminate\Http\Request;
 
-class CategoriasController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+class CategoriasController extends Controller{
+    public function index(){
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function crear(){
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function grabar(){
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Categorias  $categorias
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Categorias $categorias)
-    {
-        //
+    public function editar(){
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Categorias  $categorias
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Categorias $categorias)
-    {
-        //
+    public function cambiar(){
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Categorias  $categorias
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Categorias $categorias)
-    {
-        //
-    }
+    public function borrar(){
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Categorias  $categorias
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Categorias $categorias)
-    {
-        //
     }
 }
