0fc75d8903ce4686f0d2dd7daca44a3e2e74b929
devmeireles <dev.meireles@gmail.com>
2019-03-27 17:25:26 +0000

    :pencil2: add routes to products

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::group(['middleware' => ['auth', 'web']], function () {
-    Route::get('/dashboard', 'HomeController@index')->name('admin.dashboard.home');
+    Route::get('panel/dashboard', 'HomeController@index')->name('admin.dashboard.home');
+
+    //rutas para productos
+    Route::get('panel/productos', 'ProductosController@index')->name('admin.productos.index');
+    Route::get('panel/productos/crear', 'ProductosController@crear')->name('admin.productos.crear');
+    Route::post('panel/productos/grabar', 'ProductosController@grabar')->name('admin.productos.grabar');
+    Route::get('panel/productos/editar/{id}', 'ProductosController@editar')->name('admin.productos.editar');
+    Route::post('panel/productos/cambiar/{id}', 'ProductosController@cambiar')->name('admin.productos.cambiar');
+    Route::get('panel/productos/borrar/{id}', 'ProductosController@borrar')->name('admin.productos.borrar');
+
 });
 
 
