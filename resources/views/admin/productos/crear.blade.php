2689a2f329a69e9ac4dfb4ceb403f2006ae2c4c1
devmeireles <dev.meireles@gmail.com>
2019-04-04 21:16:24 +0000

    :pencil2: inclusion del titulo

---
+++ resources/views/admin/productos/crear.blade.php
@@ -0,0 +1,12 @@
+@include('admin.layouts.header')
+@include('admin.layouts.sidebar')
+
+<div class="main-content">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+        </div>
+    </div>
+</div>
+
+
+@include('admin.layouts.footer')
